2ccc91e412aee8d37096656ff46ba4970ca473b8
Iulia Ilies <iulia.ilies01@gmail.com>
2024-11-22 18:00:13 +0200

    Handle CORS issues

--- go.mod
+++ go.mod
@@ -2,8 +2,10 @@ module soa-gateway
 
 go 1.21.4
 
+require github.com/joho/godotenv v1.5.1
+
 require (
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
-	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -19,14 +20,24 @@ func main() {
 	}
 
 	// Base URL for the backend microservice
-    // backendURL := "http://localhost::8081"
-	backendURL := "http://host.docker.internal:8081"
+    backendURL := "http://localhost:8081"
+	// backendURL := "http://host.docker.internal:8081"
 
 	// Create a reverse proxy for the backend
 	backendProxy := newReverseProxy(backendURL)
 
 	// Set up the router
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"}, 
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		ExposedHeaders:   []string{"Content-Length"},
+		AllowCredentials: true, 
+		MaxAge:           300,  
+	}))
+
 	r.Use(middleware.Logger) // Log all requests
 
 	// Public route for login
